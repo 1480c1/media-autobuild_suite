5e59ede863ab62ac1ef657089f87fef8cabe7e4e
Christopher Degawa <ccom@randomderp.com>
2020-03-11 23:29:54 +0000

    redshift: remove perl
    mingw-w64 perl conflicts with msys and doesn't provide XML parser something, something ..

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2604,6 +2604,7 @@ fi
 
 _check=(bin-global/redshift.exe)
 if [[ $redshift = y ]] && do_vcs "https://github.com/jonls/redshift.git"; then
+    do_pacman_remove perl
     [[ -f configure ]] || log bootstrap ./bootstrap
     CFLAGS+=' -D_POSIX_C_SOURCE' \
         do_separate_confmakeinstall global --enable-wingdi \
