a77a38d90670820de1855e3b0a28fc2e30a85e0c
Christopher Degawa <ccom@randomderp.com>
2021-02-09 15:29:01 -0600

    bat: enable vapoursynth and liblensfun

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -127,7 +127,7 @@ libopenmpt version3 librav1e libsrt libgsm libvmaf libsvtav1
 set ffmpeg_options_full=chromaprint decklink frei0r libbs2b libcaca ^
 libcdio libflite libfribidi libgme libilbc libsvthevc ^
 libsvtvp9 libkvazaar libmodplug librtmp librubberband #libssh libtesseract libxavs ^
-libzmq libzvbi openal libcodec2 ladspa #vapoursynth #liblensfun ^
+libzmq libzvbi openal libcodec2 ladspa vapoursynth liblensfun ^
 libglslang vulkan libdavs2 libxavs2 libuavs3d
 
 :: options also available with the suite that add shared dependencies
